6ed7760d56ccd488b98c192dfd587016db13f28f
hmann <hjmann@gmail.com>
2021-11-22 16:27:20 -0700

    basic logic of calc in app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+//Functions for various math operations 
+
+//Add 
+
+function add(num1, num2) {
+return num1 + num2; 
+}
+
+//Subtract 
+
+function subtract(num1, num2) {
+    return num1 - num2; 
+}
+
+//multiply 
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+//divide
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+//calc basic function 
+
+function operate(num1, op , num2) {
+    if( op === "+"){
+        return add(num1, num2)
+    } else if (op === "-"){
+        return subtract(num1, num2);
+    } else if (op === "*") {
+        return multiply(num1, num2);
+    }else if (op === "/"){
+        return divide(num1, num2);
+    }
+}
